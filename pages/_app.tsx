96fe6d446422ac2e869d1ec35a092590e4f765ff
Avaneesh Tripathi <avaneeshtripathi93@gmail.com>
2022-03-05 17:02:14 +0400

    chore: fix repo base path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import "../styles/globals.css";
+import "@styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
+import { prefix } from "@utils/prefix";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="favicon" href="/favicon.png" />
+        <link rel="favicon" href={`${prefix}/favicon.ico`} />
         <title>Type Generator</title>
       </Head>
       <Component {...pageProps} />
