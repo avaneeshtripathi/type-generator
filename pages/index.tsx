e2e04a626420af163dd98882d58c7e74079dc82a
Avaneesh Tripathi <avaneeshtripathi93@gmail.com>
2022-03-05 07:25:25 +0400

    chore: add the logic for generator

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,62 @@ import Textbox from "@components/Textbox";
 import Button from "@components/Button";
 import styles from "@styles/Home.module.css";
 
+const getFormattedTypeLabel = (label: string) => {
+  return label
+    .split("_")
+    .map((each) => each.charAt(0).toUpperCase() + each.substring(1))
+    .join("");
+};
+
+const getTypeForObject = (
+  obj: Record<string, any>,
+  spacing: string,
+  name: string,
+  typesStrArr: string[]
+) => {
+  let typesStr = "";
+
+  typesStr += `type T${name} = `;
+
+  if (typeof obj === "object" && obj !== null && !Array.isArray(obj)) {
+    typesStr += "{\n";
+    Object.keys(obj).forEach((key) => {
+      if (typeof obj[key] === "object") {
+        if (obj[key] === null) {
+          typesStr += `${spacing}${key}: null;\n`;
+        } else if (Array.isArray(obj[key])) {
+          const keyName = getFormattedTypeLabel(`${name}_${key}`);
+          typesStr += `${spacing}${key}: T${keyName}[];\n`;
+          getTypeForObject(obj[key][0], spacing, keyName, typesStrArr);
+        } else {
+          const keyName = getFormattedTypeLabel(`${name}_${key}`);
+          typesStr += `${spacing}${key}: T${keyName};\n`;
+          getTypeForObject(obj[key], spacing, keyName, typesStrArr);
+        }
+      } else {
+        typesStr += `${spacing}${key}: ${typeof obj[key]};\n`;
+      }
+    });
+    typesStr += "}";
+  } else {
+    typesStr += `${obj === null ? "null" : typeof obj};`;
+  }
+
+  typesStrArr.push(typesStr + "\n\n");
+  return typesStrArr;
+};
+
 const Home: NextPage = () => {
   const [stringifiedJson, setStringifiedJson] = useState("");
   const [convertedData, setConvertedData] = useState("");
+  const [spacing, setSpacig] = useState("    ");
   const [conversionError, setConversionError] = useState(false);
 
   const handleKeyDown = useCallback((event) => {
     if (event.key === "Tab") {
       event.preventDefault();
       event.target.setRangeText(
-        "    ",
+        spacing,
         event.target.selectionStart,
         event.target.selectionStart,
         "end"
@@ -38,9 +84,12 @@ const Home: NextPage = () => {
     [conversionError]
   );
 
-  const getStringifiedTypes = useCallback((jsonData: Record<string, any>) => {
-    return JSON.stringify(jsonData, null, 4);
-  }, []);
+  const getStringifiedTypes = useCallback(
+    (jsonData: Record<string, any>) => {
+      return getTypeForObject(jsonData, spacing, "HelloTypes", []).join("");
+    },
+    [spacing]
+  );
 
   const onConvert = useCallback(() => {
     let jsonData;
@@ -54,6 +103,7 @@ const Home: NextPage = () => {
 
     const stringifiedTypes = getStringifiedTypes(jsonData);
     setConvertedData(stringifiedTypes);
+    setStringifiedJson(JSON.stringify(JSON.parse(stringifiedJson), null, 4));
   }, [stringifiedJson]);
 
   return (
