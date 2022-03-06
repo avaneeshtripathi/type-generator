8ba40214b688da59c220d880b729d904450b98de
Avaneesh Tripathi <avaneeshtripathi93@gmail.com>
2022-03-07 01:53:51 +0400

    feat: use curl to fetch to format curl and call using fetch api

--- pages/api/curl.ts
+++ pages/api/curl.ts
@@ -1,3 +1,4 @@
+import curlToFetch from "curl-to-fetch";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -5,7 +6,11 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    res.status(200).json({ data: "hello" });
+    const apiRequest = curlToFetch(req.body.data);
+    const [fetchRequest] = apiRequest.split(".then");
+    const response = await eval(fetchRequest);
+    const data = await response.json();
+    res.status(200).json(data);
   } catch (error: any) {
     res.status(503).json({ message: error.message });
   }
